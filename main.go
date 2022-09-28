be01d9fb22d4dcad63d0e4394946944ae8958619
leafoliage <leafoliages@gmail.com>
2022-09-28 23:09:04 +0800

    [mod] main.go

--- main.go
+++ main.go
@@ -1,14 +1,52 @@
 package main
 
-/*
+import (
+	"math/rand"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
-func main
-	get requests
-	for each request
-		parse request and get dices
-		for each dice
-			parse dice and get result
-			add up result
-		print result
+	"github.com/leafoliage/rollin-cli/models"
+)
 
-*/
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	for _, request := range os.Args[1:] {
+		diceStrs := parseRequest(request)
+		result := models.Result{}
+		for _, diceStr := range diceStrs {
+			if !isValidDiceStr(diceStr) {
+				continue
+			}
+			dice, err := parseDiceStr(diceStr)
+			if err != nil {
+				continue
+			}
+			newRes := dice.Roll()
+			result.Append(&newRes)
+		}
+		result.Show()
+	}
+}
+
+func parseRequest(reqStr string) (diceStrs []string) {
+	diceStrs = strings.Split(reqStr, "+")
+	return
+}
+
+func parseDiceStr(diceStr string) (dice models.Dice, err error) {
+	parsed := strings.Split(diceStr, "d")
+	dice.Amount, err = strconv.Atoi(parsed[0])
+	if err != nil {
+		return
+	}
+	dice.Side, err = strconv.Atoi(parsed[1])
+	return
+}
+
+func isValidDiceStr(diceStr string) bool {
+	matched, err := regexp.Match("\\d+d\\d+", []byte(diceStr))
+	return err == nil && matched
+}
